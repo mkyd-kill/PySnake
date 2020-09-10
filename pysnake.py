01d966b40e38680d427fa21df0a0152ce858c580
Romeo Namunde <romeomureithi@gmail.com>
2020-09-10 19:12:30 +0300

    My first commit

---
+++ pysnake.py
@@ -0,0 +1,12 @@
+# A simple python snake game
+# Easy to code and learn
+# Uses simple functions, variables and is fun to code
+
+# importations
+import tkinter as tk
+import math
+import random
+import pygame
+from tkinter import messagebox
+
+# Class definition
